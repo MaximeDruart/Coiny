b976cce0e56e776d7de6b9db4485c4a8e2b53085
holowzer <arthurlasnelhetic@gmail.com>
2019-12-17 16:58:25 +0100

    perfecting the ux commerce page

--- client/src/components/StorePage.jsx
+++ client/src/components/StorePage.jsx
@@ -25,6 +25,7 @@ const StorePage = ()=>{
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque eos porro consequuntur iure vero molestias perferendis eius ab vel dicta quia quas atque expedita nam saepe totam illo, fugiat animi Lorem ipsum dolor sit amet consectetur, <br/> <br/> adipisicing elit. Numquam temporibus esse minus exercitationem quod laboriosam qui iste aliquid ea. A dolorem similique quis iusto odio dicta <br/><br/> quasi et error nam Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae rerum non praesentium, fugit quod eligendi eum omnis unde, aut blanditiis doloremque, iure minus accusamus accusantium distinctio corporis officiis labore reprehenderit.</p>
                 </div>
             <Bottombar />
+            <div className="bottomCompenser"></div>
         </div>
 
     )
