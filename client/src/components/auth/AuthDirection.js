d938161908679031f742f59c3a7fb9db1b413ca8
CathyDolle <cathy.dolle@live.fr>
2019-12-17 22:18:56 +0100

    login page :lipstick:

--- client/src/components/auth/AuthDirection.js
+++ client/src/components/auth/AuthDirection.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link } from "react-router-dom"
 import { Jumbotron, Container, Button } from "react-bootstrap"
 import "./AuthDirection.scss"
-import logImg from "../img/log.svg"
 import Login from "./Login"
 import Header from "../welcomeComp/Header"
 
@@ -10,7 +9,6 @@ const AuthDirection = props => {
   return (
     <Container className='home'>
       <Header />
-      <img src={logImg} alt='log'></img>
       <Jumbotron>
         <h3>Connexion</h3>
       </Jumbotron>
