9c9c85e0f540856bfb140758c16db62ca9dd00ef
CathyDolle <cathy.dolle@live.fr>
2019-12-18 12:44:21 +0100

    margin edit :lipstick:

--- client/src/components/payment/Donation.js
+++ client/src/components/payment/Donation.js
@@ -2,6 +2,7 @@ import React from "react"
 import "./donation.scss"
 import { Link } from "react-router-dom"
 import backImg from "../img/back.svg"
+import donationImg from "../img/donationImg.svg"
 
 const Donation = () => {
   return (
@@ -10,9 +11,10 @@ const Donation = () => {
         <img src={backImg} alt="back"></img>
         <h5>Retour</h5>
       </Link>
+      <img class="donationImg" src={donationImg} alt="donation" />
       <h1>Donation pour la cagnotte de Franprix</h1>
       <div class="containDonation_montant">
-        <input type="number" placeholder="0"/>
+        <input type="number" placeholder="0" />
       </div>
       <h2>Euros</h2>
       <button>Valider</button>
