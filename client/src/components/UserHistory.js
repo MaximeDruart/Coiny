e464b327440d8f38a45905e5a47037263d31aedf
CathyDolle <cathy.dolle@live.fr>
2019-12-19 14:11:33 +0100

    style

--- client/src/components/UserHistory.js
+++ client/src/components/UserHistory.js
@@ -5,6 +5,9 @@ import { CSSTransition } from "react-transition-group"
 import uuid from "uuid"
 import "./userHistory.scss"
 import Axios from "axios"
+import Back from "./auth/Back"
+import donationImg from "./img/mydonation.svg"
+import boulangerieImg from "./img/boulangerie.svg"
 
 const UserHistory = props => {
   const { getExtendedData, userData } = useContext(LoginContext)
@@ -39,11 +42,13 @@ const UserHistory = props => {
   }, [ids, getBusinessDataForId])
 
   return (
-    <div className='user-history'>
-      <div className='history-header'>
-        <div className='text'>Vos dons</div>
+    <div className="user-history">
+      <Back history={props.history}>Go back</Back>
+      <div className="history-header">
+        <div className="text">Vos dons</div>
+        <img src={donationImg} alt="donation"></img>
       </div>
-      <div className='history-container'>
+      <div className="history-container">
         {userData &&
         ids.length === businessesData.length &&
         businessesData !== "undefined" &&
@@ -54,18 +59,22 @@ const UserHistory = props => {
                 key={uuid()}
                 in={!!userData}
                 timeout={500}
-                classNames='history-item'
+                classNames="history-item"
               >
-                <div key={uuid()} className='history-item'>
-                  <div className='amount'>{historyItem.amount}</div>
-                  <div className='businessName'>
-                    {businessesData[index].name}
+                <div key={uuid()} className="history-item">
+                  <div className="history-item_text">
+                    <div className="amount">{historyItem.amount}€ chez</div>
+                    <div className="businessName">
+                      {businessesData[index].name}
+                    </div>
+                  </div>
+                  <div className="history-item_img">
+                    <img src={boulangerieImg} alt="shop"/>
                   </div>
                 </div>
               </CSSTransition>
             ))
-          : arr.map(x => <div key={uuid()} className='placeholder'></div>)}
-        }
+          : arr.map(x => <div key={uuid()} className="placeholder"></div>)}
       </div>
     </div>
   )
